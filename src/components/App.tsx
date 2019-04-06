7c93989675a73183bda40cbfcab377cb6f2ff31c
cherner <csebranek@gmail.com>
2019-04-06 14:08:34 -0500

    using react hooks

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,7 @@
 import '../App.css';
 //import * as Data from './phrases.json'
 import {Data} from '../data/phrases.js'
-import React, {useState} from 'react'
+import React, {FunctionComponent, useEffect, useState} from 'react'
 import {JSONResult} from "csvtojson/v2/lineToJson";
 import {
   Form,
@@ -41,109 +41,111 @@ interface IState {
 
 export type Variant = "success" | "danger" | undefined
 
-class App extends React.Component<{},IState> {
+//TODO set initial state in () below.
+export const App:FunctionComponent<IState> = () => {
 
-  getRan = () => {
-    return Math.floor(Math.random() * Math.floor(this.state.subsetData.length))
+  const [entered, setEntered] = useState('');
+  const [data] = useState(Data);
+  const [subsetData, setSubsetData] = useState(Data);
+  const [pos, setPos] = useState(0);
+  const [actual, setActual] = useState(Data[0].thai);//used for next answer
+  const [answer, setAnswer] = useState(Data[0].thai)
+
+  const [english, setEnglish] = useState(Data[0].english);
+  const [feedback, setFeedback] = useState("");
+
+  const [tries, setTries] = useState(0);
+  const [score, setScore] = useState(0);
+  const [display, setDisplay] = useState('block');
+
+  const [category, setCategory] = useState('default');
+
+  const getRan = () => {
+    return Math.floor(Math.random() * Math.floor(subsetData.length))
   };
 
-  data = Data;
-
-  state = {
-      data: this.data || {},
-      subsetData: this.data || {},
-      category:'--',
-      val: '',
-      pos: 0,
-      entered:'',
-      actual:'',
-      feedback: undefined,
-      display:"none",
-      score:0,
-      tries:0,
-      english:this.data[0].english
-  }
 
   //TODO update this type as well
-  sendAnswer = (e:any) => {
+  const sendAnswer = (e:any) => {
     e.preventDefault();
-    let tries = this.state.tries + 1
-    let ans = (this.state.subsetData[this.state.pos].thai === this.state.val) ? "success" : "danger";
-    let newScore = 0;
+    setTries(tries+1);
+    setAnswer(subsetData[pos].thai);
+    let ans = (subsetData[pos].thai === entered) ? "success" : "danger";
     if (ans == "success"){
-      newScore = this.state.score+1
+      setScore(score+1);
     } else {
-      if (this.state.score > 0) {
-        newScore = this.state.score - 1
+      if (score > 0) {
+        setScore(score-1);
       }
     }
     let display = "block"
-    let p = this.getRan();
+    let p = getRan();
     console.log(p)
-    console.log(this.state.subsetData[this.state.pos])
-    this.setState({
-      actual:this.state.subsetData[this.state.pos].thai,
-      entered:this.state.val,
-      english:this.state.subsetData[this.state.pos].english,
-      pos:p,
-      val:'',
-      feedback:ans as Variant,
-      display:display,
-      score:newScore,
-      tries:tries
-    });
+    console.log(subsetData[pos])
+
+    setPos(p);
+    setActual(subsetData[p].thai);
+    setEnglish(subsetData[p].english);
+    setFeedback(ans);
+    setDisplay(display);
 
   };
 
   //TODO update the type to use React.FormEvent<HTMLInputElement>
   // https://github.com/DefinitelyTyped/DefinitelyTyped/issues/16208
-  handleChange = (event:any) => {
-    this.setState({val:event.currentTarget.value});
+  const handleChange = (event:any) => {
+    setEntered(event.currentTarget.value)
   };
 
-  changeCategory = (event:any) => {
+  //do this then set other things
+  useEffect(() => {
+    console.log('using effect');
+    let p = getRan();
+    setPos(p);
+    setEnglish(subsetData[p].english)
+  },[subsetData]);
+
+  const changeCategory = (event:any) => {
     //change the subset here and use it!
     let subset = [];
     let selectedCategory = event.currentTarget.value;
     if (selectedCategory === 'default'){
-      this.setState({subsetData:this.state.data}, ()=> {
-        let p = this.getRan();
-        this.setState({
-          pos:p,
-          english:this.state.subsetData[p].english,
-          category:selectedCategory,
-        })
-        console.log(p)
-        console.log(this.state.subsetData)
-      })
+      setSubsetData(data)
+      let p = getRan();
+      setPos(p);
+      setEnglish(subsetData[pos].english);
+      setCategory(selectedCategory);
       return;
     }
 
-    //TODO change for loop
-    for (let i=0; i < Object.keys(this.state.data).length; i++){
-      console.log(this.state.data[i]);
-      if (selectedCategory === this.state.data[i].category){
-        subset.push(this.state.data[i])
+    //TODO change for loop and make async using useEffect hook
+    for (let i=0; i < Object.keys(data).length; i++){
+      console.log('calculating subset...')
+      if (selectedCategory === data[i].category){
+        subset.push(data[i])
       }
     }
-    this.setState({category:event.currentTarget.value})
-    this.setState({subsetData:subset},() => {
-      let p = this.getRan();
-      this.setState({pos:p});
-      this.setState({english:this.state.subsetData[p].english})
-      console.log(p)
-      console.log(this.state.subsetData)
-    });
+
+    setCategory(selectedCategory);
+    console.log(selectedCategory);
+    console.log('setting subset...');
+    setSubsetData(subset)
+    console.log(subsetData)
+    console.log('subset set...');
+    let p = getRan();
+    setCategory(selectedCategory);
+    console.log('english...',english)
+    console.log(p)
+    console.log(subsetData)
   }
 
-  render() {
     return (
 
-        <Form onSubmit={this.sendAnswer} style={{maxWidth:'500px'}}>
-          <h3>New word: {this.state.english}</h3>
+        <Form onSubmit={sendAnswer} style={{maxWidth:'500px'}}>
+          <h3>New word: {english}</h3>
           <Form.Group controlId="exampleForm.ControlSelect1">
             <Form.Label>Category</Form.Label>
-            <Form.Control as="select" onChange={this.changeCategory} value={this.state.category}>
+            <Form.Control as="select" onChange={changeCategory} value={category}>
               <option value="default">All</option>
               <option value="general">General</option>
               <option value="dining">Dining</option>
@@ -151,13 +153,12 @@ class App extends React.Component<{},IState> {
               <option value="object">Object</option>
             </Form.Control>
           </Form.Group>
-          <FormControl type="text" name="val" onChange={this.handleChange} value={this.state.val} />
-          <Alert style={{display: this.state.display}}  variant={this.state.feedback}><FormText>You entered: {this.state.entered} and answer was {this.state.actual}</FormText></Alert>
+          <FormControl type="text" name="val" onChange={handleChange} value={entered} />
+          <Alert style={{display: display}}  variant="success"><FormText>You entered: {entered} and answer was {answer}</FormText></Alert>
           <Button variant="primary" type="submit">Submit</Button>
-          <Alert style={{float:"right"}} variant="info">Score: {this.state.score} / {this.state.tries}</Alert>
+          <Alert style={{float:"right"}} variant="info">Score: {score} / {tries}</Alert>
         </Form>
     );
-  }
 }
 
 export default App;
